7f56a8453ab0d5d370df90fbdebce01942d9384e
Brenden Taul <89364653+btaul@users.noreply.github.com>
2022-07-07 13:59:11 -0400

    starting springboot project

---
+++ snowbunnies/src/main/java/com/cognizant/snowbunnies/SnowbunniesApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.snowbunnies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowbunniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowbunniesApplication.class, args);
+	}
+
+}
