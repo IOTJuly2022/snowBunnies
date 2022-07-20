1593975546cf16f7bfbd832433c909d72720caa5
tate-partain <43040487+tate-partain@users.noreply.github.com>
2022-07-20 16:17:26 -0400

    Added basics to User and Cart Product
    Variables with getters/setters

---
+++ snowbunnies/src/main/java/com/cognizant/snowbunnies/models/CartProduct.java
@@ -0,0 +1,29 @@
+package com.cognizant.snowbunnies.models;
+
+public class CartProduct {
+
+	private long cartId;
+	private long productId;
+	
+	
+	public long getCartId() {
+		return cartId;
+	}
+	public void setCartId(long cartId) {
+		this.cartId = cartId;
+	}
+	public long getProductId() {
+		return productId;
+	}
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	
+	public void addCartProduct() {
+		//????
+	}
+	
+	public void removeCartProduct() {
+		//????
+	}
+}
--- snowbunnies/src/main/java/com/cognizant/snowbunnies/models/User.java
+++ snowbunnies/src/main/java/com/cognizant/snowbunnies/models/User.java
@@ -1,5 +1,92 @@
 package com.cognizant.snowbunnies.models;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class User {
+	
+	private String username;
+	private String password;
+	private int status;
+	private Cart userCart;
+	private PaymentDetail paymentDetails;
+	private List<Invoice> invoiceList;
+	
+	
+
+
+	public User() {
+		super();
+	}
+
+	public User(String username, String password, int status) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.status = status;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+//		if(user status > a number or the number theyre trying to set) {
+			this.status = status;
+//		}
+	}
+
+	public Cart getUserCart() {
+		return userCart;
+	}
+
+	public void setUserCart(Cart userCart) {
+		this.userCart = userCart;
+	}
+
+	public PaymentDetail getPaymentDetails() {
+		return paymentDetails;
+	}
+
+	public void setPaymentDetails(PaymentDetail paymentDetails) {
+		this.paymentDetails = paymentDetails;
+	}
+
+	public List<Invoice> getInvoiceList() {
+		return invoiceList;
+	}
+
+	public void setInvoiceList(List<Invoice> invoiceList) {
+		this.invoiceList = invoiceList;
+	}
+
+	public void addInvoice(Invoice inv) {
+		if(invoiceList != null) {
+			invoiceList.add(inv);
+		} else {
+			invoiceList = new ArrayList<>();
+			invoiceList.add(inv);
+		}
+	}
+	
+	
+	
+	
+
+} //user class
