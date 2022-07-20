bb20c535f3a9d878b6793bb0f50df48c9b2d554b
Sidoniam <38336172+Sidoniam@users.noreply.github.com>
2022-07-20 11:06:15 -0500

    billing detail model update

--- snowbunnies/src/main/java/com/cognizant/snowbunnies/models/BillingDetail.java
+++ snowbunnies/src/main/java/com/cognizant/snowbunnies/models/BillingDetail.java
@@ -1,5 +1,119 @@
 package com.cognizant.snowbunnies.models;
 
-public class BillingDetail {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "delivery")
+
+public class BillingDetail {
+	
+	@Id
+	@Column(nullable = false, unique = true)
+	@GeneratedValue
+	private long deliveryDetailID;
+	
+	@Column(nullable = false)
+	private String streetAddress;
+	
+	@Column(nullable = false)
+	private String streetAddressTwo;
+	
+	@Column(nullable = false)
+	private String state;
+	
+	@Column(nullable = false)
+	private String country;
+	
+	@Column(nullable = false)
+	private int zipcode;
+	
+	@Column(nullable = false)
+	private String city;
+	
+	
+	public BillingDetail(long deliveryDetailID, String streetAddress, String streetAddressTwo, String state,
+			String country, int zipcode, String city) {
+		super();
+		this.deliveryDetailID = deliveryDetailID;
+		this.streetAddress = streetAddress;
+		this.streetAddressTwo = streetAddressTwo;
+		this.state = state;
+		this.country = country;
+		this.zipcode = zipcode;
+		this.city = city;
+	}
+
+
+	public long getDeliveryDetailID() {
+		return deliveryDetailID;
+	}
+
+
+	public void setDeliveryDetailID(long deliveryDetailID) {
+		this.deliveryDetailID = deliveryDetailID;
+	}
+
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+
+	public String getStreetAddressTwo() {
+		return streetAddressTwo;
+	}
+
+
+	public void setStreetAddressTwo(String streetAddressTwo) {
+		this.streetAddressTwo = streetAddressTwo;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	} 
 }
