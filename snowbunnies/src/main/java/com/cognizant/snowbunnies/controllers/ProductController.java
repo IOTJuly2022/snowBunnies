ff2b59cab5d550d8f7bb563acf75d0dc058bd1de
Brenden Taul <89364653+btaul@users.noreply.github.com>
2022-08-22 14:30:58 -0400

    set up product and invoice controller

--- snowbunnies/src/main/java/com/cognizant/snowbunnies/controllers/ProductController.java
+++ snowbunnies/src/main/java/com/cognizant/snowbunnies/controllers/ProductController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.snowbunnies.models.Product;
 import com.cognizant.snowbunnies.repositories.ProductRepository;
 
+@RestController
 public class ProductController {
 	
 	@Autowired
