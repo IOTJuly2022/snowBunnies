1aff37d54b54d059d5e29af2b119d8a07e3079b0
Faycal Fedad <fedad.faycal.dz@gmail.com>
2022-08-22 22:37:50 -0400

    updated

--- front-end-angular/src/app/cart/cart.component.ts
+++ front-end-angular/src/app/cart/cart.component.ts
@@ -1,3 +1,19 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-cart',
+//   templateUrl: './cart.component.html',
+//   styleUrls: ['./cart.component.css']
+// })
+// export class CartComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 @Component({
